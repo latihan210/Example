d61084f79b82fa028ce44f729f65037ba4b5deb2
latihan210 <yosepsolahudin200@gmail.com>
2026-02-12 21:22:29 +0700

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema; // <--- 1. Tambahkan baris ini
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(191); // <--- 2. Tambahkan baris ini
+        Schema::defaultStringLength(191);
     }
 }
